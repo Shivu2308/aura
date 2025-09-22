c740403413ab88400806598b6678b85dc27b5be9
Shivu2308 <151336259+Shivu2308@users.noreply.github.com>
2025-09-22 14:12:56 +0530

    Update socket.js

--- Backend/socket.js
+++ Backend/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors : {
-        origin : 'https://aura-ecru-tau.vercel.app',
+        origin : 'https://aura-3m5s.onrender.com',
         methods:['GET', 'POST', 'DELETE']
     }
 })
