22db96f88a53c1d08ff57bbe19919f64042512d4
Shivu2308 <151336259+Shivu2308@users.noreply.github.com>
2025-09-22 13:47:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ dotenv.config();
 const port = process.env.PORT || 8000
 app.use(cors({
     origin : [
-    'http://localhost:5173'
+    'https://aura-ecru-tau.vercel.app'
   ],
     credentials:true
 }))
